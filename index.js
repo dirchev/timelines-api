ef33dae2afc8c8464e0a6884845278eb47520e82
dirchev <dirchev@camplight.net>
2018-02-23 21:29:49 +0000

    add home page and update log

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ var database = {
 app.use(cors())
 app.use(bodyParser.json())
 
+app.get('/', function (res, res) {
+  res.redirect('https://github.com/dirchev/timelines-api')
+})
+
 require('./Timeline')(app, database)
 require('./TimelineEventLinks')(app, database)
 require('./Event')(app, database)
 
-app.listen(process.env.PORT || 3001, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3001, () => console.log('Example app listening on port 3001!'))
